cc84c4df6d849ef73bc5341059bdade8ac356600
mizza-cz <mizza.cz666@gmail.com>
2023-12-15 17:01:54 +0100

    add fixes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,20 +1,34 @@
 headerNavOpenerClick();
+
 function headerNavOpenerClick() {
   const bodyEl = document.querySelector("body");
   const headerNavOpener = document.querySelector(".js-header__opener");
-  if (!bodyEl || !headerNavOpener) {
+  const headerCloser = document.querySelector(".js-header__closer"); // Изменено на .js-header__closer
+
+  if (!bodyEl || !headerNavOpener || !headerCloser) {
     return;
   }
+
   headerNavOpener.addEventListener("click", function () {
     if (!this.classList.contains("is-open")) {
       bodyEl.classList.add("is-nav-open");
       this.classList.add("is-open");
+      headerCloser.classList.remove("is-open");
     } else {
       bodyEl.classList.remove("is-nav-open");
       this.classList.remove("is-open");
     }
   });
+
+  headerCloser.addEventListener("click", function () {
+    // Добавлен обработчик для headerCloser
+    if (headerNavOpener.classList.contains("is-open")) {
+      bodyEl.classList.remove("is-nav-open");
+      headerNavOpener.classList.remove("is-open");
+    }
+  });
 }
+
 var e,
   t = window.location.hash;
 "" != t &&
